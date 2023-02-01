f7489b4d2027a4929d1f4f22dd30d56f7f9a8796
Rajyavardhan21 <rajyavardhan.rathore@edureka.in>
2023-02-01 18:05:08 +0530

    inital commit

---
+++ jsprac.js
@@ -0,0 +1,9 @@
+document.getElementById('btn').addEventListener('click', function(){
+   var valueInput =  document.getElementById('inp').value ;
+   document.getElementById('one').innerHTML = "hi there "
+//    alert(valueInput);
+
+})
+
+
+
